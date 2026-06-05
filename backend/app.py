0c716512088369543e502c5a6675eca62f5a9107
adrianlardin <adrianlardin@hotmail.com>
2026-06-05 18:29:03 +0200

    endpoints of the structure

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,11 @@ from dotenv import load_dotenv
 import os
 from database import db
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 from models.models import User, Group, GroupMember, Expense, ExpenseSplit, Settlement
+from routes.auth import auth_bp
+from routes.user import user_bp
+from routes.group import group_bp
 
 load_dotenv()
 
@@ -12,15 +16,20 @@ app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
 
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 db.init_app(app)
 migrate = Migrate(app, db)
+jwt = JWTManager(app)
+
+app.register_blueprint(auth_bp)
+app.register_blueprint(user_bp)
+app.register_blueprint(group_bp)
 
 @app.route("/")
 def home():
     return {"message": "Backend funcionando"}
-
 
 if __name__ == "__main__":
     app.run(debug=True)
