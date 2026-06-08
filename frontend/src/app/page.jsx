0f8042fe9827a08d9d34e5212d19da980ba14636
joanlitaponte <joanlitaponte@gmail.com>
2026-06-08 19:29:33 +0200

    Mejora landing page y estilos

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,24 +1,28 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import "./page.css";
 
-export default function Landing() {
-
+export default function LandingPage() {
   const router = useRouter();
 
   return (
-    <div>
+    <div className="landing-container">
+      <div className="landing-card">
+        <h1>FairShare</h1>
 
-      <h1>FairShare</h1>
+        <p>Comparte gastos de forma sencilla</p>
 
-      <button onClick={() => router.push("/Login")}>
-        Iniciar sesión
-      </button>
+        <div className="landing-buttons">
+          <button onClick={() => router.push("/Login")}>
+            Iniciar sesión
+          </button>
 
-      <button onClick={() => router.push("/Register")}>
-        Registrarse
-      </button>
-
+          <button onClick={() => router.push("/Register")}>
+            Registrarse
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
